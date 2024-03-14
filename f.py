92b672a43a721ef058a36648c537ea10f8be0816
Rabiul Islam <152082966+ria2002@users.noreply.github.com>
2024-03-14 15:41:47 +0800

    Add files via upload

---
+++ f.py
@@ -0,0 +1,22 @@
+inventory={}
+ProductNo1="Mobile Phone"
+ProductNo1_quantity=5
+ProductNo1_price=20000
+ProductNo1_releaseYear=2020
+
+inventory["ProductNo1"]= ProductNo1
+inventory["ProductNo1_quantity"]= ProductNo1_quantity
+inventory["ProductNo1_price"]= ProductNo1_price
+inventory["ProductNo1_releaseYear"]=ProductNo1_releaseYear
+
+ProductNo2 = "Laptop"
+ProductNo2_quantity = 10
+ProductNo2_price = 50000
+ProductNo2_releaseYear= 2023
+
+inventory["ProductNo2"]= ProductNo2
+inventory["ProductNo2_quantity"]= ProductNo2_quantity
+inventory["ProductNo2_price"]= ProductNo2_price
+inventory["ProductNo2_releaseYear"]=ProductNo2_releaseYear
+
+print(inventory)
